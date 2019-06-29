b26c1fa5b5b59efbc33a67f5b02323cd03be9672
sehannp <sehanp@gmail.com>
2019-06-29 18:40:42 +1000

    scripts part1

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function()
+{
+  $('#mycarousel').carousel({ interval:2000});
+  $('#carouselButton').click(function()
+  {
+    if ($('#carouselButton').children('span').hasClass('fa-pause'))
+    {
+        $('#mycarousel').carousel('pause');
+        $('#carouselButton').children('span').removeClass('fa-pause');
+        $('#carouselButton').children('span').addClass('fa-play');
+    }
+    else if ($('#carouselButton').children('span').hasClass('fa-play'))
+    {
+        $('#mycarousel').carousel('cycle');
+        $('#carouselButton').children('span').removeClass('fa-play');
+        $('#carouselButton').children('span').addClass('fa-pause');
+    }
+  });
+});
+
+
+//code for running login modal
+$(document).ready(function()
+{
+  $('#loginbutton').click(function()
+  {
+    $('#loginmodal').modal('show');
+  });
+});
+
+//code for running reservetable modal
+$(document).ready(function()
+{
+  $('#reservebutton').click(function()
+  {
+    $('#reservemodal').modal('show');
+  });
+});
+
+
+/*commenting out because not needed now script
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+*/
